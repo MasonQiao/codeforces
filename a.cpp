a25335a37875e6e3fb3c4672ece5d3dcabbe0f5d
MasonQiao <119145677+MasonQiao@users.noreply.github.com>
2023-10-04 10:06:01 -0500

    doesnt work at all

--- a.cpp
+++ a.cpp
@@ -1,1 +1,25 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n, k;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+
+	cin >> t;
+	for (int j = 0; j < t; j++) {
+		cin >> n >> k;
+
+		int ans = 0;
+
+		for (int i = 1; i <= n; i++) {
+			ans = max(ans,  2*(i/k + (i%k != 0) - max(0, 2*i - n )));
+		}
+		cout << ans << "\n";
+	}
+}
 
