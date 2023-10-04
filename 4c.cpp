7df41881a194f903c96229a9b4a2a7bfe6cf0858
MasonQiao <mqiao1@cps.edu>
2023-10-03 21:53:38 -0500

    use map to not tle

--- 4c.cpp
+++ 4c.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string usernames[100000], currentusername;
+string usernames[100000], currentusername, testusername;
 int n;
 
 bool inusernames(string username) {
@@ -17,7 +17,7 @@ int main () {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n;
-    cout << "\n";
+
     for (int i = 0; i < n; i++) {
         cin >> currentusername;
         if (!(inusernames(currentusername))) {
@@ -28,13 +28,13 @@ int main () {
             int a = 0;
             while (true) {
                 a++;
-                currentusername = currentusername + to_string(a);
-                if (!(inusernames(currentusername))) {
+                testusername = currentusername + to_string(a);
+                if (!(inusernames(testusername))) {
                     break;
                 }
             }
-            cout << currentusername << "\n";
-            usernames[i] = currentusername;
+            cout << testusername << "\n";
+            usernames[i] = testusername;
         }
     }
 
