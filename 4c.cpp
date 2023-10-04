73dbfddf588c0f0f4d56bf0d81835026bffc0d62
MasonQiao <mqiao1@cps.edu>
2023-10-03 20:59:09 -0500

    mle

--- 4c.cpp
+++ 4c.cpp
@@ -1,1 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+bool inusernames(string username, string usernames1[], int n1) {
+    for (int i = 0; i < n1; i++) {
+        if (username == usernames1[i]) {
+                return true;
+        }
+    }
+    return false;
+}
+
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    string currentusername;
+    string usernames[n];
+    cin >> n;
+    cout << "\n";
+    for (int i = 0; i < n; i++) {
+        cin >> currentusername;
+        if (!(inusernames(currentusername, usernames, n))) {
+            cout << "OK" << "\n";
+            usernames[i] = currentusername;
+        }
+        else {
+            int a = 0;
+            while (true) {
+                a++;
+                currentusername = currentusername + to_string(a);
+                if (!(inusernames(currentusername, usernames, n))) {
+                    break;
+                }
+            }
+            cout << currentusername << "\n";
+            usernames[i] = currentusername;
+        }
+    }
+
+    return 0;
+}
