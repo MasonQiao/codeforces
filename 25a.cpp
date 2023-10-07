39a0e989f624cf1c66c0911eaa55438dbb0a2526
MasonQiao <mqiao1@cps.edu>
2023-10-07 13:58:33 -0500

    doesnt work

--- 25a.cpp
+++ 25a.cpp
@@ -1,1 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int n, previous_number, next_number;
+
+int main() {
+    cin >> n;
+    int difs[n-1];
+    cin >> previous_number;
+    for (int i = 0; i < n - 1; i++) {
+        cin >> next_number;
+        difs[i] = next_number - previous_number;
+        if (i + 2 == n) {
+            break;
+        }
+        previous_number = next_number;
+
+    }
+
+    if (n==3) {
+        if (previous_number == next_number) {
+            cout << 1;
+        }
+        else if (difs[0] == -1*difs[1]) {
+            cout << 2;
+        }
+        else {
+            cout << 3;
+        }
+        return 0;
+    }
+    if (difs[0] != difs[1] && difs[1] == difs[2] && difs[2] == difs[3]) {
+        cout << 1;
+        return 0;
+    }
+
+    if (difs[n-2] != difs[n-3] && difs[n-3] == difs[n-4] && difs[n-4] == difs[n-5]) {
+        cout << n;
+        return 0;
+    }
+
+    for (int i = 1; i < n; i++) {
+        if ((difs[i-2] != difs[i-1]) && (difs[i-2] + difs[i-1] == 2*difs[i-3] || difs[i-2] + difs[i-1] == difs[i])) {
+            cout << i;
+            return 0;
+
+        }
+    }
+
+}
