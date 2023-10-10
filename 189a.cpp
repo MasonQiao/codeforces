f09e1f46a06cb657108dcb4dce245e3533c5ed55
MasonQiao <mqiao1@cps.edu>
2023-10-09 20:24:05 -0500

    tle if there are too many cases where it works

--- 189a.cpp
+++ 189a.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int n, a, b, c, arr[3], anwsers[16000000], array_index;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> arr[0] >> arr[1] >> arr[2];
+    sort(arr, arr + 3);
+    a = arr[0];
+    b = arr[1];
+    c = arr[2];
+    for (int i = n/c; i >= 0; i-=1) {
+        for (int j = 0; j >= 0; j-=1) {
+            for (int k = n/a; k >= 0; k-=1) {
+                if (i*c + j*b + k*a == n) {
+                    anwsers[array_index] = i + j + k;
+                    array_index ++;
+                }
+            }
+        }
+    }
+    sort(anwsers, anwsers + array_index+1);
+    cout << anwsers[array_index];
+}
