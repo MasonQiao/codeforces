30f6010e05c73c619ba26aabfda1fefe057f1664
MasonQiao <mqiao1@cps.edu>
2023-10-12 19:59:36 -0500

    Update 455a.cpp

--- 455a.cpp
+++ 455a.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, x, anwser, best_move, lowest_loss;
+int n, x, anwser;
+const int aux[10e5];
 map <int, int>sequence;
 set <int>possible_moves;
 set<int>::iterator itr;
@@ -15,37 +16,17 @@ int main() {
         sequence[x] ++;
         possible_moves.insert(x);
     }
+    int i = 1;
 
-    while (!possible_moves.empty()) {
-        itr = possible_moves.begin();
-        lowest_loss = (*itr+1) * (sequence[*itr + 1]) + (*itr - 1) * (sequence[*itr - 1]) - *itr;
-        best_move = *itr;
-        itr++;
-        while (true) {
-            if ((*itr+1) * (sequence[*itr + 1]) + (*itr - 1) * (sequence[*itr - 1]) - *itr < lowest_loss) {
-                best_move = *itr;
-                lowest_loss = *itr + (*itr+1) * (sequence[*itr + 1]) + (*itr - 1) * (sequence[*itr - 1]);
-            }
 
-            if (itr == possible_moves.end()) {
+    while (i++) {
+        if (map[i] == 0) {
             break;
-            }
-
-            itr ++;
         }
-
-        sequence[best_move] -= 1;
-        if (sequence[best_move] == 0) {
-            possible_moves.erase(best_move);
-        }
-        possible_moves.erase(best_move+1);
-        possible_moves.erase(best_move-1);
-        sequence[best_move+1] = 0;
-        sequence[best_move-1] = 0;
-        anwser += best_move;
+        aux[i] = map[i];
     }
+    for (int j = 1; j < i; j++) {
 
-    cout << anwser;
-
+    }
     return 0;
 }
